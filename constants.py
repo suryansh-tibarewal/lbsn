208f1651e7575cb25e276f8374f735f0efadc496
Rishabh Gupta <r4onlyrishabh@gmail.com>
2017-03-27 02:59:20 +0530

    final edit #4

--- constants.py
+++ constants.py
@@ -16,8 +16,8 @@ maxDescriptionCount = 10
 e_t0 = 0.5
 e_r0 = 0.01
 
-#e_lat = 0.3422742228921536
-#e_lon = 0.09916773323165684
+e_lat = 0.3422742228921536
+e_lon = 0.09916773323165684
 
 # p should lie between 0 and 2/3
 p = 0.3
